e32e4ba0936b5c8fc6a5b07b3e3c5085f43c2ab1
Tanmay Maheshwari <tm19534cse2019@gmail.com>
2022-07-11 20:40:18 +0530

    added profile dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
+import { FiMenu, FiX, FiLogOut, FiUser } from "react-icons/fi";
 const navitem = [
     { path: "/", name: "Home" },
     { path: "/blogs", name: "Blogs" },
@@ -8,15 +8,17 @@ const navitem = [
 ];
 function Navbar() {
     const [toggle, setToggleNav] = useState(false);
+    const [showProfile, setShowProfile] = useState(false);
     const linkRef = useRef();
     const linksContainerRef = useRef();
+    const menuRef = useRef();
     //const [height, setHeight] = useState("0px");
     function toggleHandler() {
         setToggleNav((prev) => !prev);
     }
     useEffect(() => {
         const linksHeight = linkRef.current.getBoundingClientRect().height;
-        console.log(linksHeight);
+        //console.log(linksHeight);
         if (toggle) {
             linksContainerRef.current.style.height = `${linksHeight}px`;
         } else {
@@ -24,8 +26,23 @@ function Navbar() {
         }
     }, [toggle]);
 
+    useEffect(() => {
+        const handler = (event) => {
+            if (!menuRef.current.contains(event.target)) {
+                setShowProfile(false);
+            }
+            // console.log(menuRef.current);
+            // console.log(event.target);
+        };
+        document.addEventListener("mousedown", handler);
+
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        };
+    });
+
     return (
-        <nav className="border-b p-4 relative flex justify-between md:gap-8 items-center">
+        <nav className="border-b px-6 py-2 relative flex justify-between md:gap-8 items-center">
             <h1 className="font-bold text-3xl order-2 md:order-1">Logo</h1>
 
             {
@@ -60,13 +77,42 @@ function Navbar() {
                     })}
                 </ul>
             </div>
-            <button className="h-12 w-12 overflow-hidden rounded-full focus:ring ring-offset-1 focus:ring-purple-600 order-3">
-                <img
-                    className="w-full h-full"
-                    src="https://randomuser.me/api/portraits/men/3.jpg"
-                    alt="user img"
-                />
-            </button>
+            <div className="order-3">
+                <div className="relative h-12 w-12" ref={menuRef}>
+                    <button
+                        onClick={() => setShowProfile((prev) => !prev)}
+                        className={`h-12 w-12 rounded-full ring-offset-1 ${
+                            showProfile ? "ring-purple-600 ring-2" : ""
+                        } `}
+                    >
+                        <img
+                            className="w-full h-full rounded-full"
+                            src="https://randomuser.me/api/portraits/men/3.jpg"
+                            alt="user img"
+                        />
+                    </button>
+                    <ul
+                        className="absolute top-[115%] text-black bg-white border-x border-b right-0 min-w-[256px] max-w-[300px] break-words text-center rounded-lg"
+                        style={{ display: showProfile ? "block" : "none" }}
+                    >
+                        <li className="border-t">
+                            <a
+                                className="px-2 py-4 flex items-center justify-center gap-2"
+                                href="/"
+                            >
+                                <FiUser />
+                                <span>Tanmay Maheshwari</span>
+                            </a>
+                        </li>
+                        <li className="border-t">
+                            <button className="w-full h-full flex justify-center items-center gap-2 p-2">
+                                <span>Logout</span>
+                                <FiLogOut />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 }
